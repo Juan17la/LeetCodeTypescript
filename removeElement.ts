27785d4c26d0dc7819a083ffc5f9b5c123a3a322
Jud1714 <juandiegolopezarias07@gmail.com>
2026-02-09 18:45:44 -0500

    implement removeElement function to remove all instances of a value from an array

---
+++ removeElement.ts
@@ -0,0 +1,14 @@
+function removeElement(nums: number[], val: number): number {
+    let left = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        if(nums[right] !== val){
+            nums[left] = nums[right]
+            left++
+        }
+    }
+    return left;
+};
+
+console.log(removeElement([3,2,2,3], 3))
+console.log(removeElement([0,1,2,2,3,0,4,2], 2))
